ec7b8112963bb6ec3ae3dc49f32fb109b850c942
郭心源 <xy-guo20@mails.tsinghua.edu.cn>
2022-07-10 11:02:29 +0800

    搭建了后端路由的框架

--- server/app.py
+++ server/app.py
@@ -15,9 +15,58 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 # sanity check route
 
 
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+@app.route('/model', methods=['GET'])
+def getAllModels():
+    pass
+    return jsonify('test!')
+
+
+@app.route('/model', methods=['POST'])
+def createModel():
+    pass
+    return jsonify('test!')
+
+
+@app.route('/model/<modelID>', methods=['GET'])
+def getModelInfo(modelID):
+    pass
+    return jsonify('test!')
+
+
+@app.route('/model/<modelID>/test', methods=['POST'])
+def testModel(modelID):
+    pass
+    return jsonify('test!')
+
+
+@app.route('/model/<modelID>/service', methods=['POST'])
+def changeModelStatus(modelID):
+    pass
+    return jsonify('test!')
+
+
+@app.route('/model/<modelID>/predict/quick', methods=['POST'])
+def quickPredict(modelID):
+    pass
+    return jsonify('test!')
+
+
+@app.route('/model/<modelID>/predict/batch', methods=['POST'])
+def batchPredict(modelID):
+    pass
+    return jsonify('test!')
+
+
+@app.route('/model/<modelID>/predict/batch', methods=['GET'])
+def getAllTasks(modelID):
+    pass
+    return jsonify('test!')
+
+
+@app.route('/model/<modelID>/predict/batch/<taskID>', methods=['GET'])
+def getTaskInfo(modelID, taskID):
+    pass
+    return jsonify('test!')
 
 
 if __name__ == '__main__':

94eb073bdc5fac3ee68fe25b3742d9ccfb3aac0e
郭心源 <xy-guo20@mails.tsinghua.edu.cn>
2022-07-22 19:17:49 +0800

    完成了后端获取所有模型的接口的示例

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import model
+from model import Model, MODELS
+import json
 
 
 # configuration
@@ -18,54 +19,66 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/model', methods=['GET'])
 def getAllModels():
-    pass
-    return jsonify('test!')
+    res = {}
+    res['models'] = []
+    for model in list(MODELS.values()):
+        # vars : 将python对象转为dict
+        res['models'].append(vars(model))
+    return jsonify(res)
 
 
 @app.route('/model', methods=['POST'])
 def createModel():
+    # TODO
     pass
     return jsonify('test!')
 
 
 @app.route('/model/<modelID>', methods=['GET'])
 def getModelInfo(modelID):
+    # TODO
     pass
     return jsonify('test!')
 
 
 @app.route('/model/<modelID>/test', methods=['POST'])
 def testModel(modelID):
+    # TODO
     pass
     return jsonify('test!')
 
 
 @app.route('/model/<modelID>/service', methods=['POST'])
 def changeModelStatus(modelID):
+    # TODO
     pass
     return jsonify('test!')
 
 
 @app.route('/model/<modelID>/predict/quick', methods=['POST'])
 def quickPredict(modelID):
+    # TODO
     pass
     return jsonify('test!')
 
 
 @app.route('/model/<modelID>/predict/batch', methods=['POST'])
 def batchPredict(modelID):
+    # TODO
     pass
     return jsonify('test!')
 
 
 @app.route('/model/<modelID>/predict/batch', methods=['GET'])
 def getAllTasks(modelID):
+    # TODO
     pass
     return jsonify('test!')
 
 
 @app.route('/model/<modelID>/predict/batch/<taskID>', methods=['GET'])
 def getTaskInfo(modelID, taskID):
+    # TODO
     pass
     return jsonify('test!')
 

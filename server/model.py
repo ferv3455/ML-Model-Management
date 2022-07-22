4e2fe7d4af558d999db1baf49ab584dfe54fc957
郭心源 <xy-guo20@mails.tsinghua.edu.cn>
2022-07-22 19:15:48 +0800

    添加了一个全局字典，用来保存当前的所有模型信息

--- server/model.py
+++ server/model.py
@@ -28,3 +28,10 @@ class Model:
             self.pmmlInit(file)
         elif self.type == "onnx":
             self.onnxInit(file)
+
+
+# models dictionary, created with two test models
+MODELS = {
+    'testModel1': Model('testModel1', 'This is a test model.', 'pmml', None),
+    'testModel2': Model('testModel2', 'This is another test model.', 'onnx', None)
+}

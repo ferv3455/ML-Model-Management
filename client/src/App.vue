1275849b294735b8c981e53fb0c593c22694009a
谢苑瑜 <xie-yy20@mails.tsinghua.edu.cn>
2022-07-24 23:09:12 +0800

    添加了一些全局样式，增加了一个导航栏（有logo，网页名以及通往主页的链接）

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,45 @@
 <template>
+  <nav>
+    <img id="logoImg" src="./assets/s02big.png" alt="logo">
+    <p id="navTeamName">TeAM</p>
+    <router-link id="toHomeNav" to="/">
+      <img id="homeIcon" src="./assets/homeIcon.png" alt="homeIcon">
+      <p>主页</p>
+    </router-link>
+  </nav>
   <router-view />
 </template>
+
+<style>
+#logoImg {
+  height: 75%;
+
+  margin-left: 10px;
+}
+
+nav {
+  margin: -10px;
+  height: 65px;
+  display: flex;
+  align-items: center;
+}
+
+#navTeamName {
+  font-size: 30px;
+  margin-left: 15px;
+  font-weight: bolder;
+  flex-grow: 1;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+#homeIcon {
+  height: 30px;
+}
+
+#toHomeNav {
+  display: flex;
+  align-items: center;
+  color: var(--navTextColor);
+  margin-right: 20px;
+}
+</style>

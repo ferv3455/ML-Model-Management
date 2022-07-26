d018a1adc9b3194efac7016c19a7e281c013f447
谢苑瑜 <xie-yy20@mails.tsinghua.edu.cn>
2022-07-26 20:23:16 +0800

    添加了footer，将导航栏设置为固定（滚动页面网页顶端也会显示导航栏）

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,47 @@
 <template>
-  <nav>
-    <img id="logoImg" src="./assets/s02big.png" alt="logo">
-    <p id="navTeamName">TeAM</p>
-    <router-link id="toHomeNav" to="/">
-      <img id="homeIcon" src="./assets/homeIcon.png" alt="homeIcon">
-      <p>主页</p>
-    </router-link>
-  </nav>
-  <router-view />
+  <div id="appBox">
+    <nav>
+      <img id="logoImg" src="./assets/s02big.png" alt="logo">
+      <p id="navTeamName">TeAM</p>
+      <router-link id="toHomeNav" to="/">
+        <img id="homeIcon" src="./assets/homeIcon.png" alt="homeIcon">
+        <p>主页</p>
+      </router-link>
+    </nav>
+    <div id="underNav">
+    </div>
+    <div id="appRouterView">
+      <router-view />
+    </div>
+    <footer>
+      <div id="footerBox">
+        <p> info info info </p>
+        <p> email: xxxxxxx </p>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <style>
 #logoImg {
   height: 75%;
-
   margin-left: 10px;
 }
 
 nav {
-  margin: -10px;
   height: 65px;
   display: flex;
   align-items: center;
+  position: fixed;
+  width: 100%;
+  left: 0;
+  top: 0;
+  z-index: 5201314;
+}
+
+#underNav {
+  display: block;
+  height: 65px;
 }
 
 #navTeamName {
@@ -41,5 +61,34 @@ nav {
   align-items: center;
   color: var(--navTextColor);
   margin-right: 20px;
+}
+
+footer {
+  width: 100%;
+  height: 350px;
+  background: url('./assets/footer.png');
+  text-align: center;
+}
+
+#appRouterView {
+  flex: 1;
+}
+
+#appBox {
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+}
+
+#footerBox {
+  margin-top: 200px;
+}
+
+#app {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
 }
 </style>

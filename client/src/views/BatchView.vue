6c2481cbf0948977ccf43d057274f268e23ca1ed
谢苑瑜 <xie-yy20@mails.tsinghua.edu.cn>
2022-08-01 23:11:49 +0800

    完成了Batch与taskID页面的ui设计

--- client/src/views/BatchView.vue
+++ client/src/views/BatchView.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
+  <div id="batchPageDivBox">
     <h1>
       批量任务列表页面
+      <div class="modelNow">当前模型：{{ modelID }}</div>
     </h1>
-    <p>当前模型{{ modelID }}</p>
     <table id="batchPageTaskTable">
       <tr>
         <th>任务ID</th>
         <th>任务状态</th>
       </tr>
-      <tr v-for="task in tasks" :key="task">
+      <tr v-for="task in tasks" :key="task" onmouseover="this.style.backgroundColor='var(--buttonTransColor)';"
+        onmouseout="this.style.backgroundColor='transparent'">
         <td>
           <router-link :to="{ name: 'task', params: { modelID: modelID, taskID: task.id } }">
             {{ task.id }}
@@ -19,17 +20,28 @@
       </tr>
     </table>
     <div id="batchPageAddTaskArea">
-      <p>创建新任务</p>
+      <h2>创建新任务</h2>
       <!--TODO 还未确定可输入文件的类型！（跟后端沟通后再加上）-->
       <input id="uploadPageEnterModelFile" type="file">
       <button @click="upload" id="batchPageUploadButton">添加</button>
     </div>
-    <button @click="goToPredictPage" id="BatchPageGoTopredictPage">返回部署接口页面</button>
+    <button @click="goToPredictPage" id="BatchPageGoTopredictPage" class="roundButton returnButton">
+      <img class="returnIcon" src="../assets/returnIcon.png" alt="return">
+    </button>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+
+function changeBatchPageDivBoxSize() {
+  const cont = document.getElementById('batchPageDivBox');
+  if (window.innerWidth <= 800) {
+    cont.style.width = `${window.innerWidth * 0.90}px`;
+  } else {
+    cont.style.width = '720px';
+  }
+}
 
 export default {
   data() {
@@ -67,10 +79,67 @@ export default {
   mounted() {
     // TODO
     // 从后端获取数据
+    changeBatchPageDivBoxSize();
+    window.onresize = changeBatchPageDivBoxSize;
   },
 };
 
 </script>
 
 <style>
+#batchPageDivBox {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#batchPageTaskTable {
+  min-width: 100%;
+  text-align: center;
+  border-style: solid;
+  border-color: var(--textColor);
+  border-radius: 10px;
+  border-width: 3px;
+  margin-bottom: 15px;
+}
+
+#batchPageTaskTable th {
+  border-bottom-style: solid;
+  border-color: var(--textColor);
+  border-collapse: collapse;
+  border-width: 2px;
+  padding: 7px;
+}
+
+#batchPageTaskTable td {
+  padding: 7px;
+}
+
+#batchPageAddTaskArea {
+  width: 100%;
+  flex-grow: 1;
+  margin: 10px;
+  border-style: solid;
+  border-color: var(--textColor);
+  border-radius: 10px;
+  border-width: 3px;
+  box-sizing: border-box;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#batchPageAddTaskArea h2 {
+  margin: 0;
+  font-size: 25px;
+  margin-bottom: 15px;
+}
+
+#uploadPageEnterModelFile {
+  width: 100%;
+  margin-bottom: 20px;
+}
 </style>
--- client/src/views/TaskIDView.vue
+++ client/src/views/TaskIDView.vue
@@ -1,22 +1,41 @@
 <template>
-  <div>
+  <div id="taskIDPageDivBox">
     <h1>
       任务详情页面
+      <div class="modelNow">当前模型：{{ modelID }}</div>
     </h1>
-    <p>当前模型：{{ modelID }}</p>
-    <p>当前任务：{{ taskID }}</p>
-    <p>任务状态：{{ status }} </p>
-    <div v-if="status === 'finished'">
-      <p>任务结果：</p>
-      <textarea v-model="result" id="testIDPageresult" readonly>
+    <div id="taskIDPageInfoBox">
+      <div class="taskIDPageInfoSmallBox">
+        <p class="taskIDPageInfoBoxTitle">当前任务：</p>
+        <p>{{ taskID }}</p>
+      </div>
+      <div class="taskIDPageInfoSmallBox">
+        <p class="taskIDPageInfoBoxTitle">任务状态： </p>
+        <p>{{ status }}</p>
+      </div>
+      <div v-if="status === 'finished'">
+        <p class="taskIDPageInfoBoxTitle">任务结果：</p>
+        <textarea v-model="result" id="testIDPageResult" readonly>
       </textarea>
+      </div>
     </div>
-    <button @click="goToBatchPage" id="goToBatchPageButton">返回任务列表</button>
+    <button @click="goToBatchPage" id="goToBatchPageButton" class="roundButton returnButton">
+      <img class="returnIcon" src="../assets/returnIcon.png" alt="return">
+    </button>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+
+function changeTaskIDPageDivBoxSize() {
+  const cont = document.getElementById('taskIDPageDivBox');
+  if (window.innerWidth <= 800) {
+    cont.style.width = `${window.innerWidth * 0.90}px`;
+  } else {
+    cont.style.width = '720px';
+  }
+}
 
 export default {
   data() {
@@ -40,9 +59,51 @@ export default {
   mounted() {
     // TODO
     // 从后端获取数据
+    changeTaskIDPageDivBoxSize();
+    window.onresize = changeTaskIDPageDivBoxSize;
   },
 };
 </script>
 
 <style>
+#taskIDPageDivBox {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#taskIDPageInfoBox {
+  flex-grow: 1;
+  width: 100%;
+  margin: 10px;
+  border-style: solid;
+  border-color: var(--textColor);
+  border-radius: 10px;
+  border-width: 3px;
+  box-sizing: border-box;
+  padding: 20px;
+}
+
+#taskIDPageInfoBox p {
+  margin: 0;
+  margin-bottom: 10px;
+}
+
+.taskIDPageInfoBoxTitle {
+  font-weight: bolder;
+  margin-right: 10px;
+}
+
+.taskIDPageInfoSmallBox {
+  display: flex;
+  margin: 0;
+  margin-bottom: 15px;
+}
+
+#testIDPageResult {
+  width: 100%;
+  height: 200px;
+}
 </style>

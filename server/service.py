8ce0ba8376b4b4ae77aef36a3d8e20158ca75a32
ferv3455 <sh20070228@163.com>
2022-08-02 19:37:44 +0800

    draft

--- server/service.py
+++ server/service.py
@@ -1,7 +1,18 @@
 
 class Service:
+    count = 0
+
     def __init__(self, model):
         self.model = model
+        self.status = True          # True for ON, False for OFF
+        self.id = type(self).count
+        type(self).count += 1
+
+    def predict(data):
+        return 0
+
+    def beginBatch(data):
+        return -1                    # task id
 
 
 class ServiceList:

a3f9f8b68fdb9f53426243649f567bbbf64f78f6
ferv3455 <sh20070228@163.com>
2022-08-15 11:56:05 +0800

    fix bug: batch data preprocessing

--- server/app.py
+++ server/app.py
@@ -102,12 +102,13 @@ def testModel(modelID):
         for key, value in input_data.items():
             if isinstance(value, str) and value.startswith('data:'):
                 input_data[key] = decodeFile(value)          # decode base64
-        
+
          # try to get pre_process
         pre_processer = data.getPreProcessByID(modelID)
         if (pre_processer is not None):
             for key, value in input_data.items():
-                pre_pro_module = import_module(pre_processer['path'][2:-3].replace('/', '.'))
+                pre_pro_module = import_module(
+                    pre_processer['path'][2:-3].replace('/', '.'))
                 input_data[key] = pre_pro_module.pre_process(value)
 
         print('Testing on model {}: {}'.format(modelID, input_data))
@@ -309,11 +310,12 @@ def quickPredict(modelID, serviceID):
         for key, value in input_data.items():
             if isinstance(value, str) and value.startswith('data:'):
                 input_data[key] = decodeFile(value)          # decode base64
-        
+
         pre_processer = data.getPreProcessByID(modelID)
         if (pre_processer is not None):
             for key, value in input_data.items():
-                pre_pro_module = import_module(pre_processer['path'][2:-3].replace('/', '.'))
+                pre_pro_module = import_module(
+                    pre_processer['path'][2:-3].replace('/', '.'))
                 input_data[key] = pre_pro_module.pre_process(value)
 
         result = service.predict(input_data)
@@ -344,13 +346,13 @@ def batchPredict(modelID, serviceID):
             data_gen = readZIP(batch_data, service.model.input[0]['name'])
         else:
             raise ValueError('File format not readable')
-        
+
         pre_processer = data.getPreProcessByID(modelID)
         if (pre_processer is not None):
-            for d in data_gen:
-                for key, value in d.items():
-                    pre_pro_module = import_module(pre_processer['path'][2:-3].replace('/', '.'))
-                    d[key] = pre_pro_module.pre_process(value)
+            pre_pro_module = import_module(
+                pre_processer['path'][2:-3].replace('/', '.'))
+            data_gen = ({key: pre_pro_module.pre_process(value)
+                         for key, value in d.items()} for d in data_gen)
 
         taskID = service.batch(data_gen)
         res = {'id': taskID}
